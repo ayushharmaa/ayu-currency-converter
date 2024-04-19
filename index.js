dadf4968087539ae235014030ed148c4c5d41e4f
AYUSH SHARMA <ayushsharma9216@gmail.com>
2024-04-19 23:21:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+
+const freecurrencyapi = new Freecurrencyapi('fca_live_kiV8aG4K1IVmF83PHMl8VNU6BFHXpCJYNjqJgqOX');
+
+
+convertCurrency("INR","USD",8);
+
+export async function convertCurrency(fromCurrency, toCurrency, units){
+
+    const res = await freecurrencyapi.latest({
+    base_currency: fromCurrency,
+    currencies: toCurrency
+});
+const multiplier = res.data[toCurrency];
+return multiplier*units;
+}
